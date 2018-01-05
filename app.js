2be03040c7f42739e76cdbff3fff8467246165e6
temsan89 <temsan89@gmail.com>
2018-01-06 00:06:30 +0200

    fix edit item

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ const App = (function (ItemCtrl, UICtrl) {
         document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
 
         // Edit icon click event
-        document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+        document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
     }
 
     const itemAddSubmit = function (e) {
@@ -205,8 +205,8 @@ const App = (function (ItemCtrl, UICtrl) {
         e.preventDefault();
     }
 
-    // Update item submit. Event delegation
-    const itemUpdateSubmit = function(e){
+    // Edit item. Event delegation
+    const itemEditClick = function(e){
         if(e.target.classList.contains('edit-item')){
             // Get list item id (item-0, item-1)
             const listId = e.target.parentNode.parentNode.id; // get only id from all posible
